7a017bca11d274948419728215537ec16f8861af
Your Name <your_email@example.com>
2025-03-29 21:47:20 +0100

    Revert "Updating the urdf and adding new stl for sensor integration"
    This reverts commit 144bbbf133306e3e9fab7e610bc52e96d00386d4.

--- build/urdf_and_meshes_of_the_robot/CMakeFiles/urdf_and_meshes_of_the_robot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/urdf_and_meshes_of_the_robot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/urdf_and_meshes_of_the_robot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
